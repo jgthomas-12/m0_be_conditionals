8f0aefafd02868198a5b4687124bdef85048ed00
Jesse Thomas <jgthomas3@gmail.com>
2023-02-07 19:11:45 -0700

    Add spicy.rb code

---
+++ code_challenges/spicy.rb
@@ -0,0 +1,11 @@
+integer = 17
+
+if integer % 3 == 0 && integer % 5 == 0
+    puts "FizzBuzz"
+elsif integer % 3 == 0
+    puts "Fizz"
+elsif integer % 5 == 0
+    puts "Buzz"
+else 
+    puts "#{integer}"
+end 
